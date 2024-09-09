634411fd83bc8a01668d109e272c10c21378ee95
Tarık Siler <127564990+tariksiler@users.noreply.github.com>
2024-09-09 13:32:36 +0300

    Close and chaced

--- todolist/js/todolist.js
+++ todolist/js/todolist.js
@@ -1,14 +1,33 @@
+document.querySelector("#liveToastBtn").onclick = function newElement() { 
+  let item = document.querySelector("#task").value;
 
-document.querySelector("#liveToastBtn").onclick = function() { 
-    let item = document.querySelector("#task").value;
-    if( item === "") {
-        alert("Lütfen Bir DEğer Giriniz");
-        return;
-    }
-    console.log(item)
-    let ulDOM = document.querySelector("ul#list")
-    let liDOM = document.createElement("li")
-    liDOM.innerHTML = (`${item}`)
-    ulDOM.append(liDOM)
+  if( item === "") {
+      alert("Lütfen Bir Değer Giriniz");
+      return;}
+  let ulDOM = document.querySelector("ul#list")
+  let liDOM = document.createElement("li")
+  localStorage.setItem("item", item)
+  liDOM.innerHTML = (`${localStorage.getItem("item")}`)
+  ulDOM.append(liDOM)
+
+  const myList = document.querySelector("ul");
+  let newList = document.querySelectorAll("li");
+
+  newList.forEach((item) => {
+    addCloseButton(item);
+  });
+
+  function addCloseButton(item) {
+    item.onclick = () => {
+      item.classList.toggle("checked");
+    };
+
+    const span = document.createElement("span");
+    span.textContent = "\u00D7";
+    span.className = "close";
+    item.appendChild(span);
+    span.onclick = () => {
+      item.style.display = "none";
+    };
+  }
 }
-
